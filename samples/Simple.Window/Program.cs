4485bc92902080143535fae87f6d4a887782b94b
ktlwap <149824361+ktlwap@users.noreply.github.com>
2024-04-28 12:46:00 +0200

    Re-structure project

--- samples/Simple.Window/Program.cs
+++ samples/Simple.Window/Program.cs
@@ -8,6 +8,7 @@ class Program
     static void Main(string[] args)
     {
         WebView webView = WebView.Create();
+        WebView.SetSize(webView, 800, 600, Hint.None);
         WebView.SetTitle(webView, "Simple.Window");
         WebView.Navigate(webView, "https://google.com");
         WebView.Run(webView);
